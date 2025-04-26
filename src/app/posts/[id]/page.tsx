efee5819d216e7a14a7232b665ba697af594bd25
Yong <yongjohnathonch@gmail.com>
2025-04-26 17:57:13 +0800

    chore: refactor html

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -3,10 +3,8 @@ import { Suspense } from "react";
 
 export default async function Page({ params }: { params: { id: string } }) {
   return (
-    <main className="px-5 pt-24 text-center">
-      <Suspense fallback="Loading post...">
-        <Post params={params} />
-      </Suspense>
-    </main>
+    <Suspense fallback="Loading post...">
+      <Post params={params} />
+    </Suspense>
   );
 }
--- src/components/post.tsx
+++ src/components/post.tsx
@@ -20,9 +20,9 @@ export default async function Post({ params }: { params: { id: string } }) {
   }
 
   return (
-    <>
+    <main className="px-5 pt-24 text-center">
       <h1 className="text-5xl font-semibold mb-7">{post.title}</h1>
       <p className="max-w-[700px] mx-auto">{post.body}</p>
-    </>
+    </main>
   );
 }
